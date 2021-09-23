30629204ac9fd6d7703824ab75ec401b0845eee3
Alemrv <alemrv@gmail.com>
2021-09-22 23:09:56 -0400

    -added css grid for the grid container -added js code to clear the canvas -canvas size is set by the slider initial value, still working on updating the value each time the user moves the slide

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
+const slider = document.querySelector(".range");
 const gridContainer = document.querySelector(".grid-container");
+const clear = document.querySelector(".clear");
 
-for (let index = 0; index < 256; index++) {
-    
-    let div = document.createElement("div");
-    div.classList.add("grid-element");
-    gridContainer.appendChild(div);
-    
+function sizePick(){
+    const range = parseInt(slider.value);
+    let loopRange = range * range;
+    for (let index = 0; index < loopRange; index++) {
+        let div = document.createElement("div");
+        div.classList.add("grid-element");
+        gridContainer.appendChild(div);
+    }
+    // use css variables gridContainer.style.cssText = "grid-template-columns: repeat(16, 1fr);";
 }
+sizePick();
+
+slider.addEventListener("mouseup", sizePick);
 
 const gridElements = document.querySelectorAll(".grid-element");
 
 gridElements.forEach(gridElement =>{
     gridElement.addEventListener("mouseover", () =>{
         gridElement.style.cssText = "background-color: black"; 
+    });
+    clear.addEventListener("click", () =>{
+        gridElement.style.cssText = "background-color: white";
     });
 });
