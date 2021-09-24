8cdf353de4d18274d8da1aa300a39000c098baaa
Alemrv <alemrv@gmail.com>
2021-09-23 23:02:35 -0400

    -added two more buttons to html -added envent for color changes -added js code to set and rest the grid depending on slider value

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
 const slider = document.querySelector(".range");
 const gridContainer = document.querySelector(".grid-container");
 const clear = document.querySelector(".clear");
+const h3 = document.querySelector("h3")
+const color = document.querySelector(".color");
+let colorValue = color.value;
 
-function sizePick(){
+color.addEventListener("change", () =>{
+    colorValue = color.value;
+    console.log(colorValue);
+});
+
+function main(){
     const range = parseInt(slider.value);
-    let loopRange = range * range;
+    const loopRange = range * range;
+    gridContainer.textContent = "";
+    gridContainer.style.setProperty('grid-template-columns', 'repeat(' + range + ', 1fr)');
+    h3.textContent = range + "X" + range;
+
     for (let index = 0; index < loopRange; index++) {
-        let div = document.createElement("div");
+
+        const div = document.createElement("div");
         div.classList.add("grid-element");
         gridContainer.appendChild(div);
     }
-    // use css variables gridContainer.style.cssText = "grid-template-columns: repeat(16, 1fr);";
+    if (gridContainer.hasChildNodes()) {
+
+        const gridElements = document.querySelectorAll(".grid-element");
+
+        gridElements.forEach(gridElement =>{
+
+            gridElement.addEventListener("mouseover", () =>{
+
+                gridElement.style.setProperty('background-color', colorValue);
+            });
+
+            clear.addEventListener("click", () =>{
+
+                gridElement.style.cssText = "background-color: white";
+
+            });
+        });
+    }
 }
-sizePick();
+main();
 
-slider.addEventListener("mouseup", sizePick);
+slider.addEventListener("change", main);
 
-const gridElements = document.querySelectorAll(".grid-element");
 
-gridElements.forEach(gridElement =>{
-    gridElement.addEventListener("mouseover", () =>{
-        gridElement.style.cssText = "background-color: black"; 
-    });
-    clear.addEventListener("click", () =>{
-        gridElement.style.cssText = "background-color: white";
-    });
-});
+
+
+
+
+
+
+
+
+
+
