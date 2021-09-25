6e31af2c09dedadcee47be4da23d6b12189f8702
Alemrv <alemrv@gmail.com>
2021-09-24 21:46:43 -0400

    -Added js logic for the rainbow and eraser button

--- script.js
+++ script.js
@@ -3,19 +3,42 @@ const gridContainer = document.querySelector(".grid-container");
 const clear = document.querySelector(".clear");
 const h3 = document.querySelector("h3")
 const color = document.querySelector(".color");
+const eraser = document.querySelector(".eraser");
+const rainbow = document.querySelector(".rainbow");
 let colorValue = color.value;
+
+let normal = true;
+let multipleColors = false;
 
 color.addEventListener("change", () =>{
     colorValue = color.value;
-    console.log(colorValue);
+    normal = true;
+    multipleColors = false;
 });
+
+eraser.addEventListener("click", () =>{
+    colorValue = "#ffffff";
+    normal = true;
+    multipleColors = false;
+});
+
+rainbow.addEventListener("click", () =>{
+    normal = false;
+    multipleColors = true;
+});
+
+function getRandomInt(min, max) {
+
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
 
 function main(){
     const range = parseInt(slider.value);
     const loopRange = range * range;
     gridContainer.textContent = "";
     gridContainer.style.setProperty('grid-template-columns', 'repeat(' + range + ', 1fr)');
-    h3.textContent = range + "X" + range;
+    h3.textContent = `${range} x ${range}`;
 
     for (let index = 0; index < loopRange; index++) {
 
@@ -31,7 +54,17 @@ function main(){
 
             gridElement.addEventListener("mouseover", () =>{
 
-                gridElement.style.setProperty('background-color', colorValue);
+                if (multipleColors === true) {
+                    let r = getRandomInt(1, 256);
+                    let g = getRandomInt(1, 256);
+                    let b = getRandomInt(1, 256);
+                    let rainbowColor = `rgb(${r},${b},${g})`;
+                    colorValue = rainbowColor;
+                    gridElement.style.setProperty('background-color', colorValue);
+                }
+                else{
+                    gridElement.style.setProperty('background-color', colorValue);
+                }
             });
 
             clear.addEventListener("click", () =>{
@@ -45,6 +78,8 @@ function main(){
 main();
 
 slider.addEventListener("change", main);
+
+
 
 
 
